9d4855c0fa7f6d0f5dac9c8100a91633ae62fed6
fily-gif <zeroad69@gmail.com>
2024-06-15 20:51:00 -0600

    fixed uptime (i hope) and git hash (for sure)

--- sweater/utils.py
+++ sweater/utils.py
@@ -7,8 +7,8 @@ from PIL import Image
 
 def last_githash(repo="https://github.com/fily-gif/evry.git", branch="HEAD") -> str | None:
     try:
-        git_hash =  subprocess.check_output(["git", "ls-remote", repo, branch]).decode("ascii").split().decode('ascii')
-        return git_hash[0][:7]
+        git_hash = subprocess.check_output(["git", "ls-remote", repo, branch]).decode('ascii')
+        return git_hash.split()[0][:7]
     except Exception as e:
         print(f"oops github is on fire: {e}")
         return
